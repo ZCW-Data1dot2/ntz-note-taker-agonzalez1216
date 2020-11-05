8dcbf9d9b7f97d6194e84efa84467ff6031fc356
alfonso <alfonso@zipcodes-mbp.myfiosgateway.com>
2020-11-05 13:06:33 -0500

    edit updates -AG

--- ntz.py
+++ ntz.py
@@ -1,4 +1,4 @@
-# add your code in this file
+#!/usr/bin/env python3
 import os
 import sqlite3
 from sys import argv
@@ -20,7 +20,7 @@ def remember(new_note_data):
 
 def forget(note_name):
   sql = "DELETE FROM NTZ_NOTES WHERE NOTE_NAME =?"
-  c.execute(sql, note_name)
+  c.execute(sql, (note_name,))
 
 def clear():
   sql = "DELETE FROM NTZ_NOTES"
@@ -34,26 +34,30 @@ def retrieve_all():
     print(row)
 def edit(note_name, note_data):
   select_sql = "SELECT * FROM NTZ_NOTES WHERE NOTE_NAME =?"
-  c.execute(select_sql, note_name)
+  c.execute(select_sql, (note_name,))
   result = c.fetchone()
-  note_data = result[1] + " " + note_data
-  update_sql = "UPDATE NTZ_NOTES SET NOTE_CONTENT = ? WHERE NOTE_NAME =?"
-  c.execute(update_sql,(note_name, note_data))
+  print(result[1])
+  note_data = str(result[1] + " " + note_data)
+  print(note_data)
+  update_sql = "UPDATE NTZ_NOTES SET NOTE_CONTENT =? WHERE NOTE_NAME =?"
+  c.execute(update_sql, (note_data, note_name))
+  conn.commit()
 def cli():
 #command = input('Input command, r, c, f, e or x for clear:\n')
   if argv[1] == 'r':
-    new_note_data = (argv[2], argv[3])
+    new_note_data = ((argv[2], argv[3]))
     remember(new_note_data)
-  elif argv[1] == 'e';
+  elif argv[1] == 'e':
     edit(argv[2], argv[3])
-  elif argv[1] == 'f';
+  elif argv[1] == 'f':
     forget(argv[2])
-  elif argv[1] == 'clear';
+  elif argv[1] == 'clear':
     clear()
-  elif argv[1] == 'ntz'
+  elif argv[1] == 'ntz':
     retrieve_all()
 def get_args():
    return argv
   
 # run the main function
-cli(get_args())
+cli()
+
